566d49d49de05087634b8d9544b79b0c8332ce2d
Scott McAllister <scott.mcallister@gsa.gov>
2019-10-03 15:19:37 -0400

    FIX: typo in module name

--- pipeline/train.py
+++ pipeline/train.py
@@ -55,7 +55,7 @@ def randomized_grid_search(df, pipeline, objective_metric_name='roc_auc', n_iter
                        "vectorizer__max_df": stats.uniform(.95,.3),
                        "vectorizer__sublinear_tf": [True, False],
                        "select__n_components": [10,100,200,500,1000,1500,2000,5000],
-                       "select__n_iter": stats.randints(5,1000),
+                       "select__n_iter": stats.randint(5,1000),
                        "estimator__alpha": log_uniform(-5,2),
                        "estimator__penalty": ['l2','l1','elasticnet'],
                        "estimator__loss": ['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron'],
